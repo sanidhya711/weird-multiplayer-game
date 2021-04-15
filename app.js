c65f07c82242f23b20daa9449b5b59ed3d31406b
sanidhyajain1 <sanidhyajain077@gmail.com>
2021-04-15 12:04:04 +0530

    fixed port

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 });
 
-server.listen(3000,()=>{
+server.listen(process.env.PORT,()=>{
     console.log("server started on port "+process.env.PORT);
 });
